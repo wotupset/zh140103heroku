9f04835e1fcb7e1cc3691ec5540f403926234e0d
alice <wotupset@gmail.com>
2017-01-17 22:13:31 +0800

    Update 170113v3.php

--- 170113v3.php
+++ 170113v3.php
@@ -18,6 +18,85 @@ $db = new PDO('pgsql:'.
 }catch(PDOException $e){$chk=$e->getMessage();print_r("try-catch錯誤:".$chk);}//錯誤訊息
 
 
+
+try{
+//建立table
+$sql=<<<EOT
+CREATE TABLE IF NOT EXISTS xopowo123
+(
+    c01 varchar(200) NOT NULL,
+    c02 varchar NOT NULL,
+    c03 text NOT NULL,
+    c04 integer NOT NULL,
+    ID  SERIAL PRIMARY KEY,
+    timestamp timestamp default current_timestamp
+)
+EOT;
+//IF NOT EXISTS
+$stmt = $db->prepare($sql);
+$stmt->execute();
+}catch(PDOException $e){$chk=$e->getMessage();print_r("try-catch錯誤:".$chk);}//錯誤訊息
+
+
+try{
+//列出全部table
+$sql=<<<EOT
+SELECT * FROM pg_catalog.pg_tables 
+WHERE schemaname != 'pg_catalog' 
+AND schemaname != 'information_schema';
+EOT;
+//AND schemaname != 'information_schema';
+$stmt = $db->prepare($sql);
+$stmt->execute();
+while ($row = $stmt->fetch() ) {
+  echo "\n";
+  print_r($row['tablename']);
+}
+}catch(PDOException $e){$chk=$e->getMessage();print_r("try-catch錯誤:".$chk);}//錯誤訊息
+
+
+try{
+//插入資料
+$sql=<<<EOT
+INSERT INTO xopowo123 (c01,c02,c03,c04)
+VALUES ( 'c01語りたい', 'c02字幕組', 'c03ぴょんぴょん', 04 );
+EOT;
+
+$stmt = $db->prepare($sql);
+$stmt->execute();
+
+}catch(PDOException $e){$chk=$e->getMessage();print_r("try-catch錯誤:".$chk);}//錯誤訊息
+
+
+try{
+//列出資料
+$sql=<<<EOT
+select * from xopowo123 
+ORDER BY timestamp DESC
+EOT;
+// LIMIT 10
+$stmt = $db->prepare($sql);
+$stmt->execute();
+$rows_max = $stmt->rowCount();//計數
+echo "\n";
+echo $rows_max;
+$cc=0;
+while ($row = $stmt->fetch() ) {
+  $cc++;
+  if($cc>10){
+    break;
+  }else{
+    //continue;
+  }
+    
+  echo "\n";
+  print_r($row['c01'].'/'.$row['c02'].'/'.$row['c03'].'/'.$row['c04'].'/'.$row['id'].'/'.$row['timestamp']);
+}
+}catch(PDOException $e){$chk=$e->getMessage();print_r("try-catch錯誤:".$chk);}//錯誤訊息
+
+
+
+
 try{
 //列出資料 (指定區間)
 $sql=<<<EOT
