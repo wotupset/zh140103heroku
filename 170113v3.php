4fffae9768d179c5c155e0816fd6716ec4de2328
alice <wotupset@gmail.com>
2017-01-17 22:46:22 +0800

    Update 170113v3.php

--- 170113v3.php
+++ 170113v3.php
@@ -24,7 +24,7 @@ $db = new PDO('pgsql:'.
               'password='.$dbpass.';'
               );
 //$db->query("SET TIME ZONE '+8';");//+8
-echo $db->query("show timezone;");//+8
+echo $db->exec("show timezone;");//+8
 echo "\n";
 }catch(PDOException $e){$chk=$e->getMessage();print_r("try-catch錯誤:".$chk);}//錯誤訊息
 
