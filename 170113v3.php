710cc9142c2bbdad0dd4a65dd08dd3aa5ffdbceb
alice <wotupset@gmail.com>
2017-01-17 23:08:22 +0800

    Update 170113v3.php

--- 170113v3.php
+++ 170113v3.php
@@ -25,7 +25,7 @@ $db = new PDO('pgsql:'.
               );
 $db->exec("SET TIME ZONE 'Asia/Taipei';");//+8
 foreach( $db->query("show TimeZone") as $k => $v ){
-  echo $v;//+8
+  print_r($v);//+8
   echo "\n";
 }
 }catch(PDOException $e){$chk=$e->getMessage();print_r("try-catch錯誤:".$chk);}//錯誤訊息
