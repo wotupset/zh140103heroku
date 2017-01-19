9ee15d45667bd6b880956a61dffebbcd22d67891
alice <wotupset@gmail.com>
2017-01-19 15:36:16 +0800

    Update 170113v4.php

--- 170113v4.php
+++ 170113v4.php
@@ -1,4 +1,68 @@
 <?php
 
-echo 'A file with the same name already exists. Please choose a different name and try again.' > 170113v4.txt;
+header("content-Type: application/json; charset=utf-8"); //強制
+date_default_timezone_set("Asia/Taipei");//時區設定
+//date_default_timezone_set("UTC");//時區設定
+$tz=date_default_timezone_get();
+echo 'php_timezone='.$tz."\n";
+$time=time();
+echo 'now='.date("Y-m-d H:i:s",$time)."\n";
+echo 'UTC='.gmdate("Y-m-d H:i:s",$time)."\n";
+
+try{
+$dbopts=parse_url(getenv('DATABASE_URL'));
+//print_r($dbopts);
+$dbhost = $dbopts["host"];
+$dbuser = $dbopts["user"];
+$dbpass = $dbopts["pass"];
+$dbname = ltrim($dbopts["path"],'/');
+  
+$db = new PDO('pgsql:'.
+              'dbname='.$dbname.';'.
+              'host='.$dbhost.';'.
+              'user='.$dbuser.';'.
+              'password='.$dbpass.';'
+              );
+//$db->exec("SET TIME ZONE '$tz';");//+8
+$db->exec("set timezone TO '$tz';");//+8
+  
+foreach( $db->query("show TimeZone") as $k => $v ){
+  echo 'pgsql_timezone='.$v[0]."\n";
+}
+}catch(PDOException $e){$chk=$e->getMessage();print_r("try-catch錯誤:".$chk);}//錯誤訊息
+
+
+try{
+//建立table
+$sql=<<<EOT
+CREATE TABLE IF NOT EXISTS nya123
+(
+    c01 varchar(100) NOT NULL,
+    c03 text NOT NULL,
+    c04 integer NOT NULL,
+    ID  SERIAL PRIMARY KEY,
+    timestamp timestamp default current_timestamp
+)
+EOT;
+//IF NOT EXISTS
+$stmt = $db->prepare($sql);
+$stmt->execute();
+}catch(PDOException $e){$chk=$e->getMessage();print_r("try-catch錯誤:".$chk);}//錯誤訊息
+
+
+try{
+//列出全部table
+$sql=<<<EOT
+SELECT * FROM pg_catalog.pg_tables 
+WHERE schemaname != 'pg_catalog' 
+AND schemaname != 'information_schema';
+EOT;
+//AND schemaname != 'information_schema';
+$stmt = $db->prepare($sql);
+$stmt->execute();
+while ($row = $stmt->fetch() ) {
+  echo $row['tablename']."\n";
+}
+}catch(PDOException $e){$chk=$e->getMessage();print_r("try-catch錯誤:".$chk);}//錯誤訊息
+
 ?>
