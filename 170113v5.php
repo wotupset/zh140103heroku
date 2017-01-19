ceaf8536c6fdb00e05146e8eed4d9d17c997f8e3
alice <wotupset@gmail.com>
2017-01-20 00:40:55 +0800

    Update 170113v5.php

--- 170113v5.php
+++ 170113v5.php
@@ -1,7 +1,26 @@
 <?php
+$FFF=pathinfo($_SERVER["SCRIPT_FILENAME"]);
+$phpself=$FFF['basename'];
+$phpself2=$phpself['filename'];
+/*
+Array
+(
+    [dirname] => /00/src
+    [basename] => 1406616621815.jpg
+    [extension] => jpg
+    [filename] => 1406616621815
+)
+*/
 
+
+//
 require_once('170113v4b.php');
 if( $auth != "國" ){exit;}
+$output_path=output_html($htmlbody);//回傳檔案位置
+echo $output_path;
+//$curlpost=curlpost_html($output_path);
+
+
 
 function curlpost_html($x){
 	//$output_path=$GLOBALS['output_path'];
@@ -42,6 +61,86 @@ function curlpost_html($x){
 	$x=$FFF;
 	return $x;
 }
+
+function output_html($x){
+	$time=$GLOBALS['time'];
+	$ym=date('ym',$time);//輸出的檔案名稱
+	$board_title2=$GLOBALS['board_title2'];
+	$url_num=$GLOBALS['url_num'];
+	$url_num2=$GLOBALS['url_num2'];
+	//
+	$htmlbody=$x;
+	//
+$htmlbody=<<<EOT
+<html>
+<head>
+<title>$board_title2</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<STYLE>
+img.zoom {
+height:auto; width:auto;
+min-width:20px; min-height:20px;
+max-width:250px; max-height:250px;
+border:1px solid blue;
+padding-right:5px;
+background-color:#00ffff;
+}
+span.image {
+width:250px; 
+height:250px;
+border:1px solid #000;
+display: inline-block;
+}
+span.name {
+display: inline-block;
+white-space:nowrap;
+font-weight: bold;
+color: #117743;
+min-width:10px;
+max-width:100px;
+overflow:hidden;
+}
+span.title {
+display: inline-block;
+white-space:nowrap;
+font-weight: bold;
+color: #CC1105;
+min-width:10px;
+max-width:100px;
+overflow:hidden;
+}
+span.idno {
+display: inline-block;
+white-space:nowrap;
+min-width:10px;
+max-width:500px;
+overflow:hidden;
+}
+</STYLE>
+
+</head>
+<body bgcolor="#FFFFEE" text="#800000" link="#0000EE" vlink="#0000EE">
+$htmlbody
+</body>
+</html>;
+EOT;
+
+	//
+	//$tmp=$path.'dbp_'.$time.'_'.$url_num2.'n'.$url_num.'_'.rdm_str().'.htm';
+	$tmp=$phpself2.'.htm';
+	if(is_file($tmp)){
+		unlink($tmp);
+		if(is_file($tmp)){die("刪除檔案失敗");}
+	}//舊檔案有存在就移除
+	file_put_contents($tmp, $htmlbody);
+	if(!is_file($tmp)){die("建立檔案失敗");}
+	//
+	$x=$tmp;
+	return $x;
+}
+
+
+
 
 
 ?>
