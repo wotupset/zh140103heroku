c0841539245a5ac150b3b7372201bd8187f5e059
alice <wotupset@gmail.com>
2017-01-19 18:55:59 +0800

    Update 170113v4b.php
    	//echo print_r($chat_array,true);exit;//檢查點

--- 170113v4b.php
+++ 170113v4b.php
@@ -1,8 +1,10 @@
 <?php
-require_once('curl_getinfo.php');
 $query_string=$_SERVER['QUERY_STRING'];
 $url=$query_string;
-
+//
+require_once('simple_html_dom.php');
+require_once('curl_getinfo.php');
+//
 
 if(1){
   $x=curl_FFF($url);
@@ -12,9 +14,15 @@ if(1){
   $geterror=$x_2 =$x[2];//錯誤
   //simple_html_dom
   if(!$getdata){echo print_r($getinfo,true);exit;}
-  echo print_r($getinfo,true);
+  //echo print_r($getinfo,true);
   $content=$getdata;
 }
+
+//
+$html = str_get_html($content) or die('沒有收到資料');//simple_html_dom自訂函式
+$chat_array='';
+$chat_array = $html->outertext;
+echo print_r($chat_array,true);exit;//檢查點
 
 //
 $auth="國";
