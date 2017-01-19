1161f20635ace7420c4c6670f3c0a401d58230d9
alice <wotupset@gmail.com>
2017-01-19 18:48:09 +0800

    Update 170113v4b.php

--- 170113v4b.php
+++ 170113v4b.php
@@ -1,4 +1,22 @@
 <?php
 require_once('curl_getinfo.php');
+$query_string=$_SERVER['QUERY_STRING'];
+$url=$query_string;
+
+
+if(1){
+  $x=curl_FFF($url);
+  //echo print_r($x,true);exit;
+  $getdata =$x_0 =$x[0];//資料
+  $getinfo =$x_1 =$x[1];//訊息
+  $geterror=$x_2 =$x[2];//錯誤
+  //simple_html_dom
+  if(!$getdata){echo print_r($getinfo,true);exit;}
+  echo print_r($getinfo,true);
+  $content=$getdata;
+}
+
+//
+$auth="國";
 
 ?>
