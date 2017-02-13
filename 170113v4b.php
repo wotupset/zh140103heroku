03fb7d1fb71fe92ab5c31022df88f278479bd51a
alice <wotupset@gmail.com>
2017-02-13 20:27:40 +0800

    Update 170113v4b.php

--- 170113v4b.php
+++ 170113v4b.php
@@ -232,7 +232,7 @@ if($cc==0){die('[x]blockquote');}
 		//
 		$cc1++;//計算推文數量
 	}//迴圈//
-	$htmlbody='碼'.$url."<br/>\n".$board_title2."\n"."[$cc1][$cc2]<br>\n".$htmlbody."<br>\n<br>\n";
+	$htmlbody='開始'.$url."<br/>\n".$board_title2."\n"."[$cc1][$cc2]<br>\n".$htmlbody."結束<br>\n<br>\n";
 	//echo print_r($htmlbody,true);exit;//檢查點
 	//$output_path=output_html($htmlbody);//回傳檔案位置
 	//$output_path=$output_path."?".date('ymd-His',$time);//輸出的檔案名稱;
