e21fbfdfbce481c536b6ba72b7d463a235a1bd1a
alice <wotupset@gmail.com>
2017-03-04 00:58:42 +0800

    Create 170304v0.php

---
+++ 170304v0.php
@@ -0,0 +1,31 @@
+<?php
+$query_string=$_SERVER['QUERY_STRING'];
+$url=$query_string;
+//
+require_once('simple_html_dom.php');
+require_once('curl_getinfo.php');
+//
+
+if( substr_count($url, "?res=")>0 ){
+	//ok
+}else{
+	echo "?res=";exit;
+}
+
+if(1){
+  $x=curl_FFF($url);
+  //echo print_r($x,true);exit;
+  $getdata =$x_0 =$x[0];//資料
+  $getinfo =$x_1 =$x[1];//訊息
+  $geterror=$x_2 =$x[2];//錯誤
+  //simple_html_dom
+  if(!$getdata){echo print_r($getinfo,true);exit;}
+  //echo print_r($getinfo,true);//檢查點
+  $content=$getdata;
+}
+
+//
+$html = str_get_html($content) or die('沒有收到資料');//simple_html_dom自訂函式
+$chat_array='';
+$chat_array = $html->outertext;
+//echo print_r($chat_array,true);exit;//檢查點
