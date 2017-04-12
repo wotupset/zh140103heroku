48ff38936e49f9fdfa2237bb020662b4ab36c5cf
alice <wotupset@gmail.com>
2017-04-12 22:21:52 +0800

    Update 170412v0.php

--- 170412v0.php
+++ 170412v0.php
@@ -12,7 +12,26 @@ require_once('curl_getinfo.php');
 //
 $url = 'https://pbs.twimg.com/media/C9N49FPVwAAz6xm.jpg:orig';
 $header_data = get_headers($url);
-print_r($header_data);
 	
 
+////
+$FFF='';
+$FFF.='<pre>'.print_r($header_data,true).'</pre>';
+html_body($FFF);
+exit;
+////////////////function
+
+function html_body($x){
+	//$webm_count  =$x[5];
+	//
+$x=<<<EOT
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<head>
+<body>
+$x
+</body>	
+</html>
+EOT;
 ?>
