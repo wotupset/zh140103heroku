65426820929f556e613b9ce7459a063d8875d7d5
alice <wotupset@gmail.com>
2017-05-07 22:03:06 +0800

    Update 170415v3.php

--- 170415v3.php
+++ 170415v3.php
@@ -98,8 +98,8 @@ $stmt = $db->prepare($sql);
 $stmt->execute();
 $rows_max = $stmt->rowCount();//計數
 //echo '<h3>log數='.$rows_max."</h3>\n";
-if($page > ceil($rows_max) ){
-  $page=ceil($rows_max);//floor
+if($page > floor($rows_max) ){
+  $page=floor($rows_max);//floor//ceil
 }
 //$datalist = $stmt->fetchAll();
 
