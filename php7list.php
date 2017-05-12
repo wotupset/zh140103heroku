86a00490a5bf27be949b0a06e26c087cad1a317a
alice <wotupset@gmail.com>
2017-05-13 07:15:34 +0800

    Update php7list.php

--- php7list.php
+++ php7list.php
@@ -1,4 +1,12 @@
 <?php
-list($a[], , $a[]) = [1, 2, 3];
-var_dump($a);
+try{
+	list($a[], , $a[]) = [1, 2, 3];
+	var_dump($a);
+}catch(Exception $e){
+    echo 'Caught exception: ',  $e->getMessage(), "\n";
+}finally{
+    echo "php7list -try-catch-finally";
+}
+
+
 ?>
