e45028e8beb6e14c992724e07ef43515ba447a8c
alice <wotupset@gmail.com>
2017-05-18 23:11:02 +0800

    Create pretty_filesize.php

---
+++ pretty_filesize.php
@@ -0,0 +1,17 @@
+<?php
+
+// your code goes here
+
+function pretty_filesize($size){
+	$pretty='bytes';
+	if($size > 1024){$size=$size/1024;$pretty='KB';	}
+	if($size > 1024){$size=$size/1024;$pretty='MB';	}
+	if($size > 1024){$size=$size/1024;$pretty='GB';	}
+	//
+	$size=number_format($size, 2);
+	return $str=$size.' '.$pretty;
+}
+
+//echo pretty_filesize(1859584 );
+
+?>
