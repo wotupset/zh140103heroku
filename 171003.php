189d6b6735432951f1bc0a9f6c9ff3cc5640c2cf
alice <wotupset@gmail.com>
2017-10-03 16:56:08 +0800

    Create 171003.php

---
+++ 171003.php
@@ -0,0 +1,16 @@
+<?php
+extract($_POST,EXTR_SKIP);extract($_GET,EXTR_SKIP);extract($_COOKIE,EXTR_SKIP);
+error_reporting(E_ALL & ~E_NOTICE); //所有錯誤中排除NOTICE提示
+$FFF=pathinfo($_SERVER["SCRIPT_FILENAME"]);
+$phpself  = $FFF['basename'];
+$phpself2 = $FFF['filename'];
+date_default_timezone_set("Asia/Taipei");//時區設定
+$time = (string)time();
+//
+$query_string=$_SERVER['QUERY_STRING'];
+//
+require_once('curl_getinfo.php');
+$x=curl_FFF($url);
+echo print_r($x,true);
+exit;
+?>
